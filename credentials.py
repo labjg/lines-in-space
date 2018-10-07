327da18abc3c55afe0d873b0cb83a41649bd6df1
labjg <jgilbertuk@gmail.com>
2018-10-07 17:30:38 +1100

    New code

---
+++ credentials.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+# Twitter credential for Tweepy:
+twitter_consumer_key = 'your_consumer_key'
+twitter_consumer_secret = 'your_consumer_secret'
+twitter_access_token = 'your_access_token'
+twitter_access_token_secret = 'your_access_token_secret'
