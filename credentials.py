cd886be701d251e61b2361788c2fc6af85b9242e
labjg <jgilbertuk@gmail.com>
2018-10-07 17:31:57 +1100

    Initial commit

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-# Twitter credential for Tweepy:
+# Twitter app credentials:
 twitter_consumer_key = 'your_consumer_key'
 twitter_consumer_secret = 'your_consumer_secret'
 twitter_access_token = 'your_access_token'
